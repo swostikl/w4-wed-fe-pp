05dbc2407d69e5586f600f066cb1a2c8a8692d58
Lion504 <lehtonen6677@gmail.com>
2025-11-12 14:08:56 +0200

    Iteration 2: done

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,14 +1,17 @@
 import { services } from '../data'
+import { useState } from 'react'
 import Title from './Title'
 import Service from './Service'
 
 const Services = () => {
+  const [serviceList, setServiceList] = useState(services)
+
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
 
       <div className='section-center services-center'>
-        {services.map((service) => {
+        {serviceList.map((service) => {
           return <Service {...service} key={service.id} />
         })}
       </div>
