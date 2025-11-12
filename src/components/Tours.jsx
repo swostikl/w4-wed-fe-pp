e0b0a3c1049eedcb0c170f04c5ba53029647eed3
Swostika Lama <Swostika.Lama@metropolia.fi>
2025-11-12 14:16:09 +0200

    Iteration 3 is done

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,10 +1,13 @@
 import { tours } from "../data";
 import Title from "./Title";
 import Tour from "./Tour";
+import { useState } from 'react';
 
 const Tours = () => {
+  const [toursData, setToursData]= useState(tours);
+
   return (
-    <section className="section" id="tours">
+    <section className="section tours" id="tours">
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
